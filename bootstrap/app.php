394c27254349a6acd47b4758689501515abca512
Meo-ICAR <piergiuseppe.meo@icar.cnr.it>
2026-01-05 19:03:35 +0100

    usa italiano

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,22 +1,23 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Application;
 use \Inspector\Laravel\Middleware\WebRequestMonitoring;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
- ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware) {
         // Append the middleware
-        $middleware->appendToGroup('web', WebRequestMonitoring::class)
-            ->appendToGroup('api', WebRequestMonitoring::class);
+        //   $middleware->appendToGroup('web', WebRequestMonitoring::class)
+        //      ->appendToGroup('api', WebRequestMonitoring::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
